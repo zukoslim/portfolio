b9480878e0b4fc87446f5288c940d98f19c9eafb
zukoslim <deirmama.best0@gmail.com>
2024-07-10 11:25:52 +0100

    version 3

--- components/hero.tsx
+++ components/hero.tsx
@@ -24,7 +24,6 @@ const hero = () => {
         <div className="flex justify-center relative my-20 z-10">
           <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
             <h2 className="upper tracking-widest text-xs text-center text-blue-100 max-w-80">
-              Igwe osondu
             </h2>
             <TextGenerateEffect
               words="Transforming Concepts into Seamless User Experiences"
