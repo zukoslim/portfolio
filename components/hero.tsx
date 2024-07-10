2c4181c6f9fc6c8e1ec4333ab37e7f92abda07fe
zukoslim <deirmama.best0@gmail.com>
2024-07-10 11:13:22 +0100

    new update

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,7 +1,6 @@
 // rafce
 
 import React from "react";
-// import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/textgenerate";
 import MagicButton from "./ui/MagicButton";
 import { FaLocationArrow } from "react-icons/fa";
